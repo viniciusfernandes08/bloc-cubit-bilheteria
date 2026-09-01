c8d88443df64621db6c4feae5110dfeaf0fb6774
vinicius <viniciusfernandesoliveira8@gmail.com>
2026-08-31 22:04:51 -0300

    feat: add function to work with home states

--- lib/logic/cubit/home_cubit.dart
+++ lib/logic/cubit/home_cubit.dart
@@ -1,7 +1,21 @@
+import 'package:bilheteria_panucci/services/movies_api.dart';
 import 'package:bloc/bloc.dart';
 
 part 'home_states.dart';
 
 class HomeCubit extends Cubit<HomeStates> {
   HomeCubit() : super(HomeInitial());
+  
+  final HomeService _homeService = HomeService();
+
+  Future<void> getMovies() async {
+    emit(HomeLoading());
+     
+    try {
+      final movies = _homeService.fetchMovies();
+      emit(HomeSuccess());
+    } catch (e) {
+      emit(HomeError());
+    }
+  }
 }
